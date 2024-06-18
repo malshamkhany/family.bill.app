ce8284d16ca873b8a2d713003ac3bf4198e1d0be
Mustafa Al Shamkhany <mustafa@pixelplusmedia.com>
2024-06-19 01:23:13 +0400

    response time updated 2

--- src/pages/api/billContributor/many.tsx
+++ src/pages/api/billContributor/many.tsx
@@ -3,37 +3,37 @@ import updateContributor from "@/api/features/billContribution/updateContributor
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-    success: boolean;
-    error: string;
-    data: any;
+    success?: boolean;
+    error?: string;
+    data?: any;
 };
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<Partial<Data>>
 ) {
-    if (req.method === "POST") {
-        const idsString = req.body;
-        let result = [];
-        
-        if (idsString) {
-            try {
-                const ids = JSON.parse(idsString)
-
-                if(!Array.isArray(ids)) 
-                    throw new Error("invalid object")
-
-                for (const id of ids){
-                    result.push(await getContributor(id))
-                }
-            } catch (error) {
-                return res.status(400).json({ success: false, error: `contributor bill not found: ${error.message}` });
-            }
-        } else {
-            return res.status(400).json({ error: "paramater {ids} in url is missing" });
-        }
-        return res.status(200).json({ success: true, data: result });
+    if (req.method !== "POST"){
+        return res.status(400).json({ success: false, error: "method not allowed" });
     }
 
-    return res.status(400).json({ success: false, error: "method not allowed" });
+    const idsString = req.body;
+    let result = [];
+    
+    if (idsString) {
+        try {
+            const ids = JSON.parse(idsString)
+
+            if(!Array.isArray(ids)) 
+                throw new Error("invalid object")
+
+            for (const id of ids){
+                result.push(await getContributor(id))
+            }
+        } catch (error) {
+            return res.status(400).json({ success: false, error: `contributor bill not found: ${error.message}` });
+        }
+    } else {
+        return res.status(400).json({ error: "paramater {ids} in url is missing" });
+    }
+    return res.status(200).json({ success: true, data: result });
 }
