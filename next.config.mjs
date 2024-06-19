797fe64232800b02fe0517e5d8d54ac502d1fdc4
Mustafa Al Shamkhany <malshamkhany@gmail.com>
2024-06-20 01:43:02 +0400

    export config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
     reactStrictMode: true,
     output: "export",
     basePath: "/family.bill.app",
+    images: {
+        loader: "akamai",
+        path: "",
+      },
 };
 
 export default nextConfig;
