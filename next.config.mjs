2d832ca5f04d08fec05b0f5f14e442aa4d025cc5
Mustafa Al Shamkhany <malshamkhany@gmail.com>
2024-06-21 01:49:39 +0400

    nextjs standalone

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    output: "export",
-    basePath: "/family.bill.app",
-    images: {
-        loader: "akamai",
-        path: "",
-      },
+    output: "standalone",
 };
 
 export default nextConfig;
