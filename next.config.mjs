b1f7931edb2fe9d06ff33616737ba678d13f3baa
Mustafa Al Shamkhany <malshamkhany@gmail.com>
2024-06-27 23:16:35 +0400

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    output: "export",
+  reactStrictMode: true,
+  output: "export",
+  images: {
+    loader: "akamai",
+    path: "",
+  },
 };
 
 export default nextConfig;
