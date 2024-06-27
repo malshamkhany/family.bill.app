1fd29665c4fbaf956a49c1d508b876153b9cfa4e
Mustafa Al Shamkhany <malshamkhany@gmail.com>
2024-06-28 02:40:00 +0400

    images/next optimized build rollback

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
-  basePath: "/family.bill.app",
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
